9fae99f59d9e1996b264c3d4439980358561e844
Sumter <85581209+Lost-Saint@users.noreply.github.com>
2025-08-30 00:54:33 -0400

    Update leagues in LeagueSelector component

--- src/lib/components/LeagueSelector.svelte
+++ src/lib/components/LeagueSelector.svelte
@@ -4,7 +4,7 @@
 	export let selectedLeague: string;
 	export let onLeagueChange: (league: string) => void;
 
-	const leagues = ['Dawn of the Hunt', 'HC Dawn of the Hunt', 'Standard', 'Hardcore'] as const;
+	const leagues = ['Rise of the Abyssal', 'HC Rise of the Abyssal', 'Standard', 'Hardcore'] as const;
 	let internalLeague = selectedLeague;
 
 	// Reactively watch for value changes and call the parent callback
